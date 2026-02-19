8aa6b9cf39dae0451776aeedda0a44028dc1127f
potatostore <a0105775135976@gmail.com>
2026-02-19 12:32:28 +0900

    first commit

---
+++ backend/ShoppingWeb-api/src/main/java/com/ShoppingWeb/ShoppingWeb_api/ShoppingWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.ShoppingWeb.ShoppingWeb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingWebApiApplication.class, args);
+	}
+
+}
---
+++ backend/ShoppingWeb-api/src/test/java/com/ShoppingWeb/ShoppingWeb_api/ShoppingWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShoppingWeb.ShoppingWeb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
